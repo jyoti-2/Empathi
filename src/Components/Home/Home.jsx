6ae4bdbce5f3c58172bc1d186fd0eb8bc5b89760
Jyoti Kumari <jyoti02.nitt@gmail.com>
2021-06-21 14:05:50 +0530

    -

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import HomePage from "./../../assets/HomePage.jpg";
 import { Button } from 'react-bootstrap';
 import { InputGroup } from 'react-bootstrap';
