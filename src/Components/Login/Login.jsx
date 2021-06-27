da012098ba13e71e2ca2a3c48ae7b3bfc7a7695d
Jyoti Kumari <jyoti02.nitt@gmail.com>
2021-06-28 00:21:50 +0530

    About page completed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import Signup from "../Signup/Signup";
- import './Login.css';
+import './Login.css';
+import login from './../../assets/login.png';
 
 const Login = () => {
     return (
         <div className="loign-container">
-        <h1> i am the login page </h1>
+       <img
+                    src={login}
+                    className='login-image'
+                    alt=''
+        />
             
         </div>
     )
---
+++ src/Components/Signup/Signup.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './signup.css';
+import signup from './../../assets/signup.png';
+
